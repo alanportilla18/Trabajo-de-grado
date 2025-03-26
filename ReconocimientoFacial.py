8c8a15e3b8e4a0563b89207e76ef3a925802a119
Alan Portilla <portillacastillo56@gmail.com>
2025-03-26 14:46:21 -0500

    primer avance

---
+++ ReconocimientoFacial.py
@@ -0,0 +1,52 @@
+import cv2
+import os
+
+dataPath = 'C:/Users/porti/Documents/Reconocimiento/Data'
+imagesPaths = os.listdir(dataPath)
+print('ImagesPaths= ',imagesPaths)
+
+face_recognizer = cv2.face.EigenFaceRecognizer_create()
+
+#leyendo modelo
+face_recognizer.read('ModeloEigenFace.xml')
+
+# cap = cv2.VideoCapture('tests/Video1.mp4')
+# cap = cv2.VideoCapture('tests/Video2.mp4')
+# cap = cv2.VideoCapture('tests/Video.mp4')
+# cap = cv2.VideoCapture('tests/Plinio.mp4')
+# cap = cv2.VideoCapture('tests/Plinio2.mp4')
+cap = cv2.VideoCapture('tests/Auron.mp4')
+# cap = cv2.VideoCapture('tests/Prueba.mp4')
+
+faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
+
+while True:
+    ret, frame = cap.read()
+    if ret == False: break
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    auxFrame = frame.copy()
+
+    faces = faceClassif.detectMultiScale(gray,1.3,5)
+
+    for (x,y,w,h) in faces:
+        rostro = auxFrame[y:y+h, x:x+w]
+        rostro = cv2.resize(rostro, (150,150), interpolation=cv2.INTER_CUBIC)
+        rostro = cv2.cvtColor(rostro, cv2.COLOR_BGR2GRAY)
+        rostro = rostro.reshape(1, -1) 
+        result = face_recognizer.predict(rostro)
+
+        cv2.putText(frame, '{}'.format(result),(x, y-5), 1, 1.3, (255,255,0), 1, cv2.LINE_AA)
+       
+        if result[1] < 5700:
+            cv2.putText(frame, '{}'.format(imagesPaths[result[0]]),(x, y-5), 1, 1.3, (0,255,0), 1, cv2.LINE_AA)
+            cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
+        else:
+            cv2.putText(frame,'Desconocido ',(x, y-20), 2, 0.8, (0,0,255), 1, cv2.LINE_AA)
+            cv2.rectangle(frame, (x,y), (x+w,y+h), (0,0,255), 2)
+
+    cv2.imshow('frame',frame)
+    k = cv2.waitKey(1)
+    if k == 27:
+        break
+cap.release()
+cv2.destroyAllWindows()
---
+++ entrenamientoRF.py
@@ -0,0 +1,36 @@
+import cv2
+import os
+import numpy as np
+
+dataPath = 'C:/Users/porti/Documents/Reconocimiento/Data'
+peopleList = os.listdir(dataPath)
+print('Lista de personas registrada: ',peopleList)
+
+labels = []
+facesData = []
+label = 0
+
+for nameDir in peopleList:
+    personPath = dataPath + '/' + nameDir
+    print('leyendo las imagenes')
+
+    for fileName in os.listdir(personPath):
+        print('Rosotros: ', nameDir + '/' + fileName)
+        labels.append(label)
+        facesData.append(cv2.imread(personPath+'/'+fileName,0))
+        image = cv2.imread(personPath+'/'+fileName,0)
+        # cv2.imshow('image',image)
+        # cv2.waitKey(10)
+    label = label + 1
+
+# print('labels= ',labels)
+# print('Numero de 0 = ',np.count_nonzero(np.array(labels)==0))
+# print('Numero de 1 = ',np.count_nonzero(np.array(labels)==1))
+face_recognizer = cv2.face.EigenFaceRecognizer_create()
+
+print('Entrenando... ')
+face_recognizer.train(facesData, np.array(labels))
+
+face_recognizer.write('modeloEigenFace.xml')
+print('Modelo almacenado...')
+cv2.destroyAllWindows()
